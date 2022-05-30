c709a488cde1f1969524f329a0ef06cacf23c347
Tijanana <104691452+Tijanana@users.noreply.github.com>
2022-05-30 21:53:07 +0200

    Established connection with database

--- components/reader.py
+++ components/reader.py
@@ -1,8 +1,12 @@
+from mysql import connector
+
+
 class Reader:
     def __init__(self, processable_dataset):
         self.processable_dataset = processable_dataset
 
     def SaveData(self, data):
+		connection = connector.connect(host='localhost', user='username', passwd='password', database='mysql')
         # TODO: Implement
         #  Saves data received from Replicator Receiver to the Database
         #  Saving is done by the type of dataset
